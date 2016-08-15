0bdd5e3f187ff556007b1ab776b4ed7a3edf2daa
achinthau <achintha@duosoftware.com>
2016-08-15 13:01:57 +0530

    update cutom Env Variable file and Docker file

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+#FROM ubuntu
+#RUN apt-get update
+#RUN apt-get install -y git nodejs npm nodejs-legacy
+#RUN git clone git://github.com/DuoSoftware/DVP-Templates.git /usr/local/src/templates
+#RUN cd /usr/local/src/templates; npm install
+#CMD ["nodejs", "/usr/local/src/templates/app.js"]
+
+#EXPOSE 8875
+
+FROM node:5.10.0
+RUN git clone git://github.com/DuoSoftware/DVP-Templates.git /usr/local/src/templates
+RUN cd /usr/local/src/templates;
+WORKDIR /usr/local/src/templates
+RUN npm install
+EXPOSE 8875
+CMD [ "node", "/usr/local/src/templates/app.js" ]
---
+++ config/custom-environment-variables.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "DB": {
+        "Type":"SYS_DATABASE_TYPE",
+        "User":"SYS_DATABASE_POSTGRES_USER",
+        "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
+        "Port":"SYS_SQL_PORT",
+        "Host":"SYS_DATABASE_HOST",
+        "Database":"SYS_DATABASE_POSTGRES_USER"
+    },
+
+
+    "Redis":
+    {
+        "ip": "SYS_REDIS_HOST",
+        "port": "SYS_REDIS_PORT",
+        "user": "SYS_REDIS_USER",
+        "password": "SYS_REDIS_PASSWORD"
+
+    },
+
+    "Security":
+    {
+        "ip": "SYS_REDIS_HOST",
+        "port": "SYS_REDIS_PORT",
+        "user": "SYS_REDIS_USER",
+        "password": "SYS_REDIS_PASSWORD"
+
+    },
+
+  
+    "Host":
+    {
+        "vdomain": "LB_FRONTEND",
+        "domain": "HOST_NAME",
+        "port": "HOST_USERSERVICE_PORT",
+        "version": "HOST_VERSION"
+    },
+
+    "LBServer" : {
+
+        "ip": "LB_FRONTEND",
+        "port": "LB_PORT"
+
+    },
+ 
+};
+
+//NODE_CONFIG_DIR
