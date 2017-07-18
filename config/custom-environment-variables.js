192c1f44dca875e58589adefbbac26935fdf1d5a
Pawan Sasanka <pawan@duosoftware.com>
2017-07-18 11:25:47 +0530

    Sentinel support added

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -14,7 +14,13 @@ module.exports = {
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
+
 
     },
 
