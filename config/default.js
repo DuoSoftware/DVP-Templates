5e579429b8e9376902d93ab987cc279bf1840d06
sine19 <sineth17@gmail.com>
2018-06-05 16:56:57 +0530

    clear all values
    remove all default.js and conf.js values

--- config/default.js
+++ config/default.js
@@ -4,23 +4,23 @@
 module.exports = {
     "DB": {
         "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
+        "User":"",
+        "Password":"",
         "Port":5432,
-        "Host":"localhost",
-        "Database":"dvpdb"
+        "Host":"",
+        "Database":""
     },
 
 
     "Redis":
   {
     "mode":"instance",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
+    "ip": "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -31,12 +31,12 @@ module.exports = {
     "Security":
     {
         "mode":"instance",//instance, cluster, sentinel
-        "ip": "45.55.142.207",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "sentinels":{
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port":16389,
             "name":"redis-cluster"
         }
@@ -62,29 +62,15 @@ module.exports = {
 
     "Mongo":
     {
-        "ip":"104.236.231.11",
+        "ip":"",
         "port":"27017",
-        "dbname":"dvpdb",
-        "password":"DuoS123",
-        "user":"duo",
-        "replicaset" :"104.236.231.11"
+        "dbname":"",
+        "password":"",
+        "user":"",
+        "replicaset" :""
     }
 
-    /*,
-
-    "Services" : {
-        "accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiMTdmZTE4M2QtM2QyNC00NjQwLTg1NTgtNWFkNGQ5YzVlMzE1Iiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE4OTMzMDI3NTMsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NjEyOTkxNTN9.YiocvxO_cVDzH5r67-ulcDdBkjjJJDir2AeSe3jGYeA",
-        "resourceServiceHost": "resourceservice.104.131.67.21.xip.io",
-        "resourceServicePort": "8831",
-        "resourceServiceVersion": "1.0.0.0",
-        "sipuserendpointserviceHost": "sipuserendpointservice.104.131.67.21.xip.io",
-        "sipuserendpointservicePort": "8831",
-        "sipuserendpointserviceVersion": "1.0.0.0",
-        "clusterconfigserviceHost": "clusterconfig.104.131.67.21.xip.io",
-        "clusterconfigservicePort": "8831",
-        "clusterconfigserviceVersion": "1.0.0.0"
-    }
-*/
+   
 
 
 };
